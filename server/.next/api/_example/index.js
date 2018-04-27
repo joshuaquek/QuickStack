8a3821c84ec04d38fbf36a1b809a8f1d6af080da
Joshua Quek <joshua.quek@accenture.com>
2018-04-27 15:43:35 +0800

    removed built files

--- server/.next/api/_example/index.js
+++
@@ -1,7 +0,0 @@
-// 
-exports.init = function(server, app){
-  const controller = require('./controller')
-  // ---- ROUTES ----
-  controller.exampleOne('/example_1', server, app)
-  controller.exampleTwo('/example_2', server, app)
-}
--- server/.next/api/index.js
+++
@@ -1,5 +0,0 @@
-//
-exports.init = (server, app) => {
-  require('./_defaults').init(server, app) // NextJS Default Route Handling
-  require('./_example').init(server, app)
-};
