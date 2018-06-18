abaf8b0507dec3956bd4624abd8fda153112f861
Joshua Quek <joven2468@gmail.com>
2018-06-19 00:56:09 +0800

    Added Easy State Management and Data fetching onload

--- client/pages/_example/index.js
+++ client/pages/_example/index.js
@@ -1,13 +1,31 @@
-import Layout from '../../components/_example/layout'
-// import Link from 'next/link'
+import { view, store } from 'react-easy-state'
+import axios from 'axios'
+import Layout from '../components/_example/layout'
 
-export default (props) => (
+const user = store({ name: 'Bob' })
+const onChange = ev => (user.name = ev.target.value)
+
+const Page = (props) => (
   <div>
     <Layout>
       <center>
         <h1>This is an example page.</h1>
-        <p>{JSON.stringify(props)}</p>
+        <p>{JSON.stringify(props)}</p><br /><br />
+
+        <h2>Easy State Management Demo:</h2>
+        <input value={user.name} onChange={onChange} />
+        <div>Hello {user.name}!</div><br /><br />
+
+        <h2>Initial Data Fetch onComponentLoad Demo:</h2>
+        <div> Next stars: { props.stars }</div>
       </center>
     </Layout>
   </div>
 )
+
+Page.getInitialProps = async ({ req }) => {
+  const res = await axios.get('https://api.github.com/repos/zeit/next.js')
+  return { stars: res.data.stargazers_count }
+}
+
+export default view(Page)
