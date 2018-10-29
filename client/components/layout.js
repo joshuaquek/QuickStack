00edfbe62586b76a5871fe1cf3e40a72335f33b3
Joshua Quek <joven2468@gmail.com>
2018-10-30 01:05:56 +0800

    fixed favicon

--- client/components/layout.js
+++ client/components/layout.js
@@ -9,6 +9,7 @@ export default ({children, title = 'This is the default title'}) => (
       <title>{title}</title>
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <link rel='shortcut icon' type='image/png' href='/static/favicon.ico' />
     </Head>
     <header>
       <nav>
