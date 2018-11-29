c9a46e226e5a9f1578caddade90c5b517359fa72
Joshua Quek <joven2468@gmail.com>
2018-11-30 03:15:43 +0800

    removed old API folder

--- server/api_old/example/index.js
+++
@@ -1,7 +0,0 @@
-// @flow
-exports.init = function (server, app) {
-  const controller = require('./controller') // Initialise the controller
-  // ---- ROUTES ----
-  server.get('/example_1/:someRandomStuffHere/:someCoolId', controller.exampleOne(app)) // Use the controller with any route that you specify
-  server.get('/example_2', controller.exampleTwo()) // Use the controller with any route that you specify
-}
